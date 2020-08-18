db00d5d7493296b1317d6324d5749fe82556bb77
chasingsnail <wyp402905456@hotmail.com>
2020-08-18 23:51:31 +0800

    分享ppt 增加静态编译部分

--- 封装库&笔记/前端模块化分享/模块化Code/esModule/base.js
+++ 封装库&笔记/前端模块化分享/模块化Code/esModule/base.js
@@ -2,6 +2,22 @@ import { var1, var2 } from './moduleA'
 import * as vars from './moduleB'
 import m from './moduleC'
 
+// error
+if (isDev) {
+	import './moduleA.js'
+} else {
+	import './moduleB.js'
+}
+// error
+const path = './moduleA.js'
+import moduleA from path;
+// correct
+const path = './moduleA.js'
+import(path);
+
+
+
+
 export default {
 	var1: 1,
 	var2: 2,
